6f8e3516b1c88db0527c473327c31e33f039cd9b
enesozmus <ensozmus@gmail.com>
2024-04-24 17:46:29 +0300

    Initial Commit

---
+++ SwiftUI-Tinder-Clone/SwiftUI_Tinder_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Tinder_CloneApp.swift
+//  SwiftUI-Tinder-Clone
+//
+//  Created by enesozmus on 24.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Tinder_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
